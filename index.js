368d339e56d99df5acc79dea01e63ad7317b7f26
jameskk-syy <ronaldoeden70@gmail.com>
2024-09-01 08:33:48 +0300

    updated

--- index.js
+++ index.js
@@ -56,9 +56,7 @@ app.post('/ussd', async (req, res) => {
                 response = `END Invalid bid amount. Please try again.\n`;
             } else {
                 const selectedAuction = result[selectedAuctionIndex];
-                response = `CON choose payment method 
-                1 Airtel Money
-                2 MOMO`;
+                response = `CON choose payment method \n 1 Airtel Money \n2 MOMO`;
                 // Here, you can add the logic to save the bid amount to the database.
             }
         }
