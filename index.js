d4367bad4f22369eae8a32e24f093eecd020442d
jameskk-syy <ronaldoeden70@gmail.com>
2024-09-01 06:05:45 +0300

    updated

--- index.js
+++ index.js
@@ -26,34 +26,49 @@ app.post('/ussd', async (req, res) => {
         text
     } = req.body;
 
-    // Split the text input by "*" to parse the user's input at different stages
     const textArray = text.split("*");
+    const currentPage = textArray[0] === "" ? 1 : parseInt(textArray[0], 10); // Determine the current page from the user's input
+    const result = await getAuctions();
+    const itemsPerPage = 5;
 
-    // First request
-    if (text == "") {
+    if (text === "") {
         response = `CON Welcome to Laisi Reverse Auctions \n\n`;
-        response += `Bid on our live auctions:\n`;
-        const result = await getAuctions();
-        result.forEach((resq, index) => {
-            response += `${index + 1}. ${resq.auctionName}\n`;
-        });
-        response += `\nEnter the number of the auction you want to bid on:`;
-    } else {
-        // User has selected an auction
-        const selectedAuctionIndex = parseInt(textArray[0]) - 1;
-        const result = await getAuctions();
-        if (selectedAuctionIndex >= 0 && selectedAuctionIndex < result.length) {
-            const selectedAuction = result[selectedAuctionIndex];
-            response = `CON You selected ${selectedAuction.auctionName}\n`;
-            response += `Please enter your bid amount:`;
+        response += listAuctions(result, 1, itemsPerPage);
+    } else if (textArray.length === 1) {
+        if (textArray[0] === "#") {
+            response = `CON ${listAuctions(result, currentPage + 1, itemsPerPage)}`;
         } else {
-            response = `CON Invalid selection. Please try again.\n`;
+            const selectedAuctionIndex = parseInt(textArray[0], 10) - 1;
+            if (selectedAuctionIndex >= 0 && selectedAuctionIndex < result.length) {
+                const selectedAuction = result[selectedAuctionIndex];
+                response = `CON You selected ${selectedAuction.auctionName}\n`;
+                response += `Please enter your bid amount:`;
+            } else {
+                response = `CON Invalid selection. Please try again.\n`;
+            }
         }
     }
 
     res.set('content-type', 'text/plain');
     res.send(response);
 });
+
+function listAuctions(auctions, page, itemsPerPage) {
+    const start = (page - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const paginatedAuctions = auctions.slice(start, end);
+
+    let response = "Bid on our live auctions:\n";
+    paginatedAuctions.forEach((auction, index) => {
+        response += `${start + index + 1}. ${auction.auctionName}\n`;
+    });
+
+    if (end < auctions.length) {
+        response += `\n#. Next page\n`;
+    }
+
+    return response;
+}
 
 async function getAuctions() {
     const data = [];
